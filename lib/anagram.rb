2b949b583809f30e056757e1bd54df825b2aba6b
Michael Kim <m.kim4247@gmail.com>
2018-10-13 18:02:36 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,13 @@ class Anagram
   end 
   
   def match(word_list)
-    
+    desired = []
+    check = word_list.collect do |word|
+      if word.split("").sort == @word.split("").sort 
+        desired << word 
+      end 
+    end 
+    desired 
   end 
   
 end 
